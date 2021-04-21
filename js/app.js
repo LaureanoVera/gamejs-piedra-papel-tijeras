a78a59ed15753437e92c5e71dd655aa9bb29a97b
LaureanoVera <laureanoigvera@gmail.com>
2021-04-20 22:53:05 -0300

    comentarios a app.js añadidos

--- js/app.js
+++ js/app.js
@@ -5,22 +5,26 @@ if (userName == '' || userName == null) {
 }
 
 // ----------- Game -----------
-const gameResults = ["Tie", "Victory", "Defeat"];
-const gameOptions = ["Rock", "Paper", "Scissors"];
-const gameLogic = [
+const gameResults = ["Tie", "Victory", "Defeat"]; // game conditions
+const gameOptions = ["Rock", "Paper", "Scissors"]; // game options
+const gameLogic = [ // game matriz
   [0, 1, 2],
   [2, 0, 1],
   [1, 2, 0]
 ];
 
 const startGame = (user) => {
+  // game options cpu
   const cpuLogic = Math.floor(Math.random() * 3);
   const cpuSelect = gameOptions[cpuLogic];
 
+  // game options user
   const userSelect = gameOptions[user];
 
+  // game result
   const resultNum = gameLogic[cpuLogic][user];
   const resultTxt = gameResults[resultNum];
 
+  // write in index.html
   writeDom(cpuSelect, userSelect, resultTxt);
 }
